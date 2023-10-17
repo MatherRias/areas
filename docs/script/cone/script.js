909b1e10df8c0973967d017141f5254d490ceca1
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-10-17 01:20:55 +0000

    add formulas

---
+++ docs/script/cone/script.js
@@ -0,0 +1,4 @@
+function calculate(radius, slant_height){
+    let result = Math.PI * Math.pow(radius, 1) * slant_height
+    return result
+}
---
+++ docs/script/sphere/script.js
@@ -0,0 +1,4 @@
+function calculate(radius){
+    let result = 4 * Math.PI * Math.pow(radius, 2)
+    return result
+}
